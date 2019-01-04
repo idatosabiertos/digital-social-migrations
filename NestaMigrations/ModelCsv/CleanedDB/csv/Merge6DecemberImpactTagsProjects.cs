97cdb2c7f122c927d5b5a9e1a7ca4947c40567b8
Jonathan Amigo <jonathan.amigo.torres@gmail.com>
2019-01-03 23:48:23 -0300

    new files

---
+++ NestaMigrations/ModelCsv/CleanedDB/csv/Merge6DecemberImpactTagsProjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestaMigrations.ModelCsv.CleanedDB.csv
+{
+    public class Merge6DecemberImpactTagsProjects
+    {
+        public string idUnico { get; set; }
+        public string idInitiative { get; set; }
+        public string nameInitiative { get; set; }
+        public string nameOrg { get; set; }
+        public string idOrg { get; set; }
+        public string country { get; set; }
+        public string idCountry { get; set; }
+        public string city { get; set; }
+        public string idCity { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string rangoEmpleados { get; set; }
+        public string orgCount { get; set; }
+        public string projCount { get; set; }
+    }
+}
